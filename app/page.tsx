8e2f260597a429d904eadeb8427dc5fed3719c5f
sallam atmeza <atmezasallam@gmail.com>
2025-11-18 20:55:54 +0200

    basic setup, shadcn ui , theme-provider

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/components/ui/theme-toggle";
+ 
 
 export default function Home() {
-  return <div>
-<h1 className="fon-bold text-blue-500">welcomeeeeeeeeeee</h1>
+  return( <div className="p-5 min-h-screen bg-background text-foreground">
+
+<div className="W-100 flex justify-end">
+  <ThemeToggle />
+</div>
+
+<h1 className=" text-gray-500 font-barlow">welcomeeeeeeeeeee</h1>
+<h1 className=" text-white-500 ">welcomeeeeeeeeeee</h1>
+
+<Button variant="destructive">
+  click here
+</Button>
 
 
-
-
-  </div>;
+  </div>
+  
+);
 }
